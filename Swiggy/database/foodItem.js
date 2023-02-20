49c0eebdf66d0094b332e10a8777d33295a3bfe9
Biju Saji <67651024+BiJu-02@users.noreply.github.com>
2023-02-20 16:45:59 +0530

    Add files via upload

---
+++ Swiggy/database/foodItem.js
@@ -0,0 +1,62 @@
+module.exports = {
+    "mcSpicy": {
+        restoList: [
+            {
+                restoName: "McD",
+                price: 120,
+            },
+        ]
+    },
+    "fries": {
+        restoList: [
+            {
+                restoName: "McD",
+                price: 70,
+            },
+            {
+                restoName: "Burger King",
+                price: 50,
+            },
+        ]
+    }, 
+    "mcCheese": {
+        restoList: [
+            {
+                restoName: "McD",
+                price: 100,
+            },
+        ]
+    },
+    "chicken pizza": {
+        restoList: [
+            {
+                restoName: "Domino",
+                price: 170,
+            },
+            {
+                restoName: "Pizza Hut",
+                price: 180,
+            },
+        ]
+    },
+    "veg pizza": {
+        restoList: [
+            {
+                restoName: "Domino",
+                price: 90,
+            },
+            {
+                restoName: "Pizza Hut",
+                price: 100,
+            },
+        ]
+    },
+    "peperoni pizza": {
+        restoList: [
+            {
+                restoName: "Domino",
+                price: 180,
+            },
+        ]
+    },
+}
---
+++ Swiggy/database/resto.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "McD": {
+        address: "123",
+        delTime: 30,
+        menu: ["mcSpicy", "fries", "mcCheese"],
+        delFee: 11,
+        offer: {
+            minCartVal: 70,
+            discntPrcnt: 20,
+            maxDiscnt: 150
+        }
+    },
+    "Domino": {
+        address: "246",
+        delTime: 34,
+        menu: ["chicken pizza", "veg pizza", "peperoni pizza"],       
+        delFee: 12,
+    },
+    "Pizza Hut": {
+        address: "769",
+        delTime: 40,
+        menu: ["chicken pizza", "veg pizza"],
+        delFee: 10,
+    },
+}
